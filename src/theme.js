b9974e9c76b0283deea0bd196b40da901431aa85
Alex Tolstikov <tolstikovalexander2@gmail.com>
2022-10-13 21:38:45 +0300

    add dependencies

---
+++ src/theme.js
@@ -0,0 +1,59 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#2a2a2a',
+    accentText: '#af9191',
+    background: '#fff',
+    primary: '#59c',
+    thcolor: '#62d6d2',
+    secondary: '#757',
+    accent: '#d0eaea',
+    active: 'green',
+    inactive: 'red',
+  },
+
+  sizes: {
+    s: 16,
+    normal: '25%',
+    large: '35%',
+  },
+
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  shadows: {
+    normal: '1px 0px 1px 1px rgba(163, 156, 156, 0.6)',
+    medium: '2px 1px 3px 2px rgba(104, 100, 100, 0.4)',
+  },
+
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
